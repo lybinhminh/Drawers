5603f93397f04bba52f8748c5a9392cd7978a1b0
unfish <86614249+lybinhminh@users.noreply.github.com>
2024-06-10 18:11:27 +0700

    Update Pair.java

--- Pair.java
+++ Pair.java
@@ -1,8 +1,22 @@
-public class Pair <T1, T2>{
+
+public class Pair <T1, T2> extends Object{
     public T1 first;
     public T2 second;
     public Pair(T1 key, T2 value){
         first = key;
         second = value;
     }
+    @Override
+    public boolean equals(Object b){
+        if(!(b instanceof Pair))return false;
+        Pair another = (Pair)b;
+        return first.equals(another.first) && second.equals(another.second);
+    }
+    public T1 getKey(){
+        return first;
+    }
+    public T2 getValue(){
+        return second;
+    }
+
 }
